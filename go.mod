cedf035203bbcf9619d3b96544337eff5d55b7fd
mojila <me.mojila@gmail.com>
2023-10-09 14:30:34 +0700

    intial commit

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/mojila/casbin-loader
+
+go 1.21.0
+
+require (
+	github.com/casbin/casbin-go-client v1.0.3
+	google.golang.org/grpc v1.58.2
+)
+
+require (
+	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
+	github.com/casbin/casbin-server v0.0.0-20210215093047-8fcf00d431b3 // indirect
+	github.com/casbin/casbin/v2 v2.0.2 // indirect
+	github.com/casbin/gorm-adapter/v2 v2.0.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/jinzhu/gorm v1.9.10 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/lib/pq v1.1.1 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230711160842-782d3b101e98 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	casbin "github.com/casbin/casbin-go-client/client"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	ctx := context.Background()
+
+	client, err := casbin.NewClient(
+		ctx,
+		"localhost:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	connectionString := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	enforcer, err := client.NewEnforcer(ctx, casbin.Config{
+		DriverName:    "postgres",
+		ConnectString: connectionString,
+		DbSpecified:   true,
+	})
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(2)
+	}
+
+	// Load the policy from DB.
+	err = enforcer.LoadPolicy(ctx)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(3)
+	}
+
+	fmt.Println("Policy loaded from DB.")
+	os.Exit(0)
+}
